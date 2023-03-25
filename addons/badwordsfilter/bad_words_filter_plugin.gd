d10cf2dfbfaefc8fa9538e766371b073318efbac
rockgem <63050183+rockgem@users.noreply.github.com>
2023-03-25 23:57:17 +0800

    addd

---
+++ addons/badwordsfilter/bad_words_filter_plugin.gd
@@ -0,0 +1,11 @@
+tool
+extends EditorPlugin
+
+
+func _enter_tree():
+	add_autoload_singleton("BadWordsFilter", "res://addons/badwordsfilter/bad_words_filter.gd")
+
+
+func _exit_tree():
+	remove_autoload_singleton("BadWordsFilter")
+
